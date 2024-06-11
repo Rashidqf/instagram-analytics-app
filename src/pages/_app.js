dc5f7c6345e99a46292db64622b12bcf676025a4
Rashidqf <166512114+Rashidqf@users.noreply.github.com>
2024-06-11 16:20:32 +0500

    ...

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,9 @@ import { SessionProvider } from "next-auth/react";
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
-      <><header class="bg-white">
-  <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <>
+        <header class="bg-white">
+          {/* <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
     <div class="flex lg:flex-1">
       <a href="#" class="-m-1.5 p-1.5">
         <span class="sr-only">Your Company</span>
@@ -75,9 +76,9 @@ export default function App({ Component, pageProps }) {
         </div>
       </div>
     </div>
-  </div>
-</header>
-{/* 
+  </div> */}
+        </header>
+        {/* 
       <nav>
         <a href="/">Home</a>
         <a href="/auth/signin">Sign In</a>
